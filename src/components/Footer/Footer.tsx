d83d33c8e6eec9d3eecd3fe2d7109f3ee01bff5a
moshawa <moshawa0@gmail.com>
2024-05-21 15:51:43 -0400

    sticky footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,9 @@
 export default function Footer() {
   return (
-    <footer className="bg-slate-950 px-4 py-24">
-      <span className="text-7xl text-white/90">Joud</span>
+    <footer className="relative h-[800px] bg-slate-950">
+      <div className="fixed bottom-0 flex h-[800px] w-full items-center justify-center">
+        <span className="text-7xl text-white/90">Joud</span>
+      </div>
     </footer>
   )
 }
