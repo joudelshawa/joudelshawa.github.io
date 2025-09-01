aa828c24cda884714a334e62ea52f7a500cfbf2b
Mahmoud Shawa <mahmoud@shawa.dev>
2025-09-01 12:21:53 -0400

    Update text bubble data for user introduction

--- src/data/text-bubbles.ts
+++ src/data/text-bubbles.ts
@@ -1,8 +1,7 @@
 const textBubbleData = [
   `Hi, I’m Joud!`,
-  `I’m an undergraduate student at Western University pursuing an
-  Honors Specialization in Computer Science.`,
-  `I’m set to graduate in June 2024, and am passionate about
+  `I’m a masters student in Computer Science at Western University pursuing the Vector Institute Collaborative AI Specialization`,
+  `I’m set to graduate in June 2026, and am passionate about
   artificial intelligence (AI) and its potential to revolutionize
   the world we live in. 💡`,
   `Over the past few years, I have worked on a variety of personal
