6f499b4f1d4b132b6bc2c436449dce5ade039f6a
Mahmoud Shawa <mahmoud@shawa.dev>
2026-01-06 22:07:43 -0500

    fixes

--- src/data/text-bubbles.ts
+++ src/data/text-bubbles.ts
@@ -1,5 +1,5 @@
 const textBubbleData = [
-  `Hi, I’m Joud!`,
+  `Hi, I'm Joud!`,
   `I’m a masters student in Computer Science at Western University pursuing the Vector Institute Collaborative AI Specialization`,
   `I’m set to graduate in June 2026, and am passionate about
   artificial intelligence (AI) and its potential to revolutionize
